fedb76ac8caaf31c13903952fbea5d502bf467f5
sprayseal <61347128+sprayseal@users.noreply.github.com>
2020-02-23 21:26:11 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const circle = document.querySelector('.circle-object');
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
-const move = e => {
-	circle.style.top = e.clientY + 'px';
-	circle.style.left = e.clientX + 'px';
-};
-
-circle.addEventListener('mousedown', () => {
-	window.addEventListener('mousemove', move);
-});
-
-window.addEventListener('mouseup', () => {
-	window.removeEventListener('mousemove', move);
-});
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
